64031a6768ff25811119ac7c8092e86706aefdba
Joket_abc <15005771700@qq.com>
2022-02-10 10:07:57 +0800

    修复颜色

--- src/main/java/net/coreprotect/utility/Util.java
+++ src/main/java/net/coreprotect/utility/Util.java
@@ -224,7 +224,7 @@ public class Util extends Queue {
             pagination.append(Color.GREY + ")");
         }
 
-        return message.append(Color.WHITE + backArrow + Color.DARK_AQUA + Phrase.build(Phrase.LOOKUP_PAGE, Color.WHITE + page + "/" + totalPages) + nextArrow + pagination).toString();
+        return message.append(Color.WHITE + backArrow + Color.DARK_AQUA + Phrase.build(Phrase.LOOKUP_PAGE, Color.DARK_AQUA + page + "/" + totalPages) + Color.WHITE + nextArrow + pagination).toString();
     }
 
     public static String getTimeSince(long resultTime, long currentTime, boolean component) {
